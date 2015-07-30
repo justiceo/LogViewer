9409180b42b0d23663eaf166b2f4343c1bea13c0
justiceo <ebesco@yahoo.com>
2015-07-30 17:01:57 -0400

    adding mainViewModel, installed a theme library

---
+++ LogViewer/MainViewModel.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.CommandWpf;
+using Newtonsoft.Json.Linq;
+
+namespace LogViewer
+{
+	public class MainViewModel : INotifyPropertyChanged
+	{
+		#region INotifyPropertyChanged Members
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		#endregion
+
+		#region Private Fields
+		
+		private IJEnumerable<JObject> _jObjectCollection; 
+
+		private int _start = 0;
+
+		private const int ItemCount = 50;
+
+		private string _sortColumn = "Id";
+
+		private bool _ascending = true;
+
+		private int _totalItems = 0;
+
+		private ICommand _firstCommand;
+
+		private ICommand _previousCommand;
+
+		private ICommand _nextCommand;
+
+		private ICommand _lastCommand;
+
+		#endregion
+
+		public MainViewModel()
+		{
+			LoadLogEntries();
+		}
+
+		/// <summary>
+		/// The list of logEntries in the current page.
+		/// </summary>
+		public IJEnumerable<JObject> JObjectCollection
+		{
+			get
+			{
+				return _jObjectCollection;
+			}
+			private set
+			{
+				if (ReferenceEquals(_jObjectCollection, value) != true)
+				{
+					_jObjectCollection = value;
+					NotifyPropertyChanged("JObjectCollection");
+				}
+			}
+		}
+		
+		/// <summary>
+		/// Gets the information for text block in the navigation control
+		/// </summary>
+		public string GetNavigationInfo
+		{
+			get
+			{
+				if(DataStore.IsHydrated)
+					return DataStore.PageNavigationString();
+				
+				return "No Pagination Data";
+			}
+		}
+
+		/// <summary>
+		/// Gets the command for moving to the first page of products.
+		/// </summary>
+		public ICommand FirstCommand
+		{
+			get
+			{
+				if (_firstCommand == null)
+				{
+					_firstCommand = new RelayCommand
+					(
+						() =>
+						{
+							JObjectCollection = DataStore.FirstPage();
+							NotifyAll();
+						},
+						DataStore.HasPreviousPage
+					);
+
+				}
+
+				return _firstCommand;
+			}
+		}
+
+		/// <summary>
+		/// Gets the command for moving to the previous page of products.
+		/// </summary>
+		public ICommand PreviousCommand
+		{
+			get
+			{
+				if (_previousCommand == null)
+				{
+					_previousCommand = new RelayCommand
+					(
+						() =>
+						{
+							JObjectCollection = DataStore.PreviousPage();
+							NotifyAll();
+						},
+						DataStore.HasPreviousPage
+					);
+				}
+
+				return _previousCommand;
+			}
+		}
+
+		/// <summary>
+		/// Gets the command for moving to the next page of products.
+		/// </summary>
+		public ICommand NextCommand
+		{
+			get
+			{
+				if (_nextCommand == null)
+				{
+					_nextCommand = new RelayCommand
+					(
+						() =>
+						{
+							JObjectCollection = DataStore.NextPage();
+							NotifyAll();
+						},
+						DataStore.HasNextPage
+					);
+				}
+
+				return _nextCommand;
+			}
+		}
+
+		/// <summary>
+		/// Gets the command for moving to the last page of products.
+		/// </summary>
+		public ICommand LastCommand
+		{
+			get
+			{
+				if (_lastCommand == null)
+				{
+					_lastCommand = new RelayCommand
+					(
+						() =>
+						{
+							JObjectCollection = DataStore.LastPage();
+							NotifyAll();
+						},
+						DataStore.HasNextPage
+					);
+				}
+
+				return _lastCommand;
+			}
+		}
+
+		/// <summary>
+		/// The number of items to display in a page
+		/// </summary>
+		public int PageSize {
+			get { return DataStore.UserDefinedPageSize; }
+			set
+			{
+				DataStore.UserDefinedPageSize = value;
+				JObjectCollection = DataStore.RefreshPage();
+				NotifyAll();
+			}
+		}
+
+		public int[] PageSizeOptions
+		{
+
+			get
+			{
+				if (DataStore.IsHydrated)
+				{
+					List<int> possibleOptions = new List<int> { 50, 100, 200, 500, 1000, 5000, 10000 };
+					possibleOptions = possibleOptions.Where(s => s < DataStore.TotalRecordCount).ToList();
+					return possibleOptions.ToArray();
+				}
+
+				return new[] {0};
+			}
+		}
+
+		/// <summary>
+		/// Sorts the list of products.
+		/// </summary>
+		/// <param name="sortColumn">The column or member that is the basis for sorting.</param>
+		/// <param name="ascending">Set to true if the sort</param>
+		public void Sort(string sortColumn)
+		{
+			_sortColumn = sortColumn;
+
+			JObjectCollection = DataStore.ApplySort(_sortColumn);
+			NotifyAll();
+		}
+		
+		/// <summary>
+		/// Refreshes the list of log entries. Called by navigation commands.
+		/// </summary>
+		private void LoadLogEntries()
+		{
+			DataStore.LoadCollection();
+			JObjectCollection = DataStore.FirstPage();
+			NotifyAll();
+		}
+
+		/// <summary>
+		/// Notifies subscribers of changed properties.
+		/// </summary>
+		/// <param name="propertyName">Name of the changed property.</param>
+		private void NotifyPropertyChanged(string propertyName)
+		{
+			if (PropertyChanged != null)
+			{
+				PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+
+		private void NotifyAll()
+		{
+			NotifyPropertyChanged("GetNavigationInfo");
+		}
+	}
+}
---
+++ LogViewer/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+using Microsoft.Win32;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace LogViewer
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private DataGridColumn _currentSortColumn;
+
+		private ListSortDirection _currentSortDirection;
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			DataContext = new MainViewModel();
+		}
+
+		/// <summary>
+		/// Opens a log file
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void OpenLogFileHandler(object sender, RoutedEventArgs e)
+		{
+			// Create an instance of the open file dialog box.
+			OpenFileDialog openFileDialog = new OpenFileDialog();
+
+			// Call the ShowDialog method to show the dialog box. Process input if the user clicked OK.
+			if (openFileDialog.ShowDialog() == false) return;
+			if (string.IsNullOrWhiteSpace(openFileDialog.FileName)) return;
+
+			string[] fileAsStringArray = File.ReadAllLines(openFileDialog.FileName);
+			IEnumerable<JObject> jObjectsList = fileAsStringArray.Select(JsonConvert.DeserializeObject<JObject>);
+			
+			LogDataGrid.ItemsSource = jObjectsList.ToList().AsJEnumerable();
+			
+		}
+
+		/// <summary>
+		/// Defers sorting to the Data Access layer and simply updates grid with results
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void LogDataGrid_OnSorting(object sender, DataGridSortingEventArgs e)
+		{
+			e.Handled = true;
+			MainViewModel mainViewModel = (MainViewModel)DataContext;
+			mainViewModel.Sort(e.Column.SortMemberPath);
+		}
+
+		/// <summary>
+		/// Force sortable on all columns regardless of whether they're nullable.
+		/// Since the sorting operation is abstracted to the Data Access layer which takes care of edge cases.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void LogDataGrid_OnAutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
+		{
+			if (!e.Column.CanUserSort)
+				e.Column.CanUserSort = true;
+		}
+		
+		/// <summary>
+		/// Closes the application
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void ExitHandler(object sender, RoutedEventArgs e)
+		{
+			Application.Current.Shutdown();
+		}
+
+		private void PageSizeCombobox_OnLoaded(object sender, RoutedEventArgs e)
+		{
+			// set the default page size
+			PageSizeCombobox.SelectedValue = PageSizeCombobox.Items.GetItemAt(1).ToString();
+		}
+	}
+}
+
+
+
